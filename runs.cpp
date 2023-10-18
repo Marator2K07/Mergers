9b74114a418b3e76277ac6d335da43a04ae6cb91
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-10-18 19:57:58 +0300

    Начальная реализация метода создания файла с случайными цифрами

--- runs.cpp
+++ runs.cpp
@@ -1,5 +1,16 @@
 #include "runs.h"
 
+Runs::openRandomSeq(QString path,
+                    QString name,
+                    int lenght,
+                    int seed)
+{
+    QString filePath = path + '/' + name;
+    QFile file(filePath);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return;
+}
+
 Runs::Runs()
 {
 }
--- runs.h
+++ runs.h
@@ -1,12 +1,23 @@
 #ifndef RUNS_H
 #define RUNS_H
 
+#include <QFile>
+#include <QTextStream>
+
 ///
 /// \brief The Runs class
 /// Класс серий - упорядоченных послевательностей
 class Runs
 {
 public:
+    ///
+    /// \brief openRandomSeq
+    /// создает файл со случайным набором чисел
+    static openRandomSeq(QString path,
+                         QString name,
+                         int lenght,
+                         int seed);
+
     Runs();
 };
 
