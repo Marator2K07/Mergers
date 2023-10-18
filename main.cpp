db9ea8def7d73c524b3ed072cdd1ac74815fb599
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-10-18 20:12:26 +0300

    Тестовая реализация main метода

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <QCoreApplication>
 
+#include "runs.h"
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    Runs::openRandomSeq("D:","outR.txt", 22);
 
     return a.exec();
 }
