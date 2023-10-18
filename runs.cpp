8896f44f9c07a78e205c30d440194ac6756bfa07
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-10-18 19:40:05 +0300

    Новый класс Runs - для работы с упорядоченными последовательностями.

---
+++ runs.cpp
@@ -0,0 +1,5 @@
+#include "runs.h"
+
+Runs::Runs()
+{
+}
---
+++ runs.h
@@ -0,0 +1,13 @@
+#ifndef RUNS_H
+#define RUNS_H
+
+///
+/// \brief The Runs class
+/// Класс серий - упорядоченных послевательностей
+class Runs
+{
+public:
+    Runs();
+};
+
+#endif // RUNS_H
