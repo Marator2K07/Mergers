a73db4565a3379fa06adef10d335b7e899a1f784
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-10-19 10:30:21 +0300

    Тестовая реализация

--- main.cpp
+++ main.cpp
@@ -5,7 +5,12 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    Runs::openRandomSeq("D:","outR.txt", 22);
+    QFile *newFile = Runs::openRandomSeq("D:","outR.txt", 22);
+    Runner *r = new Runner();
+    Runs::setRunner(r, newFile, 0);
+
+    Runs::readIntFromRunnerPos(r, 5);
+    Runs::writeIntFromRunnerPos(r, 0, 999);
 
     return a.exec();
 }
