849a5854c0a4888dedf341cffe5ab8ababf1bbd7
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-10-19 08:57:01 +0300

    Доработка старого метода. добавление нового в класс Runs

--- runs.cpp
+++ runs.cpp
@@ -1,19 +1,47 @@
 #include "runs.h"
 
-void Runs::openRandomSeq(QString path,
-                         QString name,
-                         int lenght)
+QFile *Runs::openRandomSeq(QString path,
+                           QString name,
+                           int lenght)
 {
     QString filePath = path + '/' + name;
-    QFile file(filePath);
-    if (!file.open(QIODevice::WriteOnly  | QIODevice::Text))
-        return;
+    QFile *file = new QFile(filePath);
+    if (!file->open(QIODevice::WriteOnly  | QIODevice::Text))
+        return nullptr;
     // если все успешно, можем писать случайную информацию в файл
-    QTextStream out(&file);
+    QTextStream out(file);
     for (int i = 0; i < lenght-1; ++i) {
         int randNumber = QRandomGenerator::global()->bounded(669);
         out << randNumber << ' ';
     }
+    file->close();
+    return file;
+}
+
+void Runs::set(Runner r,
+               QFile *file,
+               qint64 pos)
+{
+    // работы с бегунком
+    r.setFile(file);
+    r.setEof(false);
+    if (r.getPos() >= 0) {
+        if (r.getPos() <= file->size()) {
+            r.setPos(pos);
+        } else {
+            r.setPos(file->size());
+        }
+    } else {
+        r.setPos(0);
+    }
+    r.setEor(r.getEof());
+    // считываем первое число из последовательности
+    if (!file->open(QIODevice::ReadOnly  | QIODevice::Text))
+        return;
+    QTextStream in(file);
+    QString firstNum;
+    in >> firstNum;
+    r.setFirst(std::stoi(firstNum));
 }
 
 Runs::Runs()
--- runs.h
+++ runs.h
@@ -1,6 +1,9 @@
 #ifndef RUNS_H
 #define RUNS_H
 
+#include "runner.h"
+
+#include <QDebug>
 #include <QFile>
 #include <QTextStream>
 #include <QRandomGenerator>
@@ -14,9 +17,13 @@ public:
     ///
     /// \brief openRandomSeq
     /// создает файл со случайным набором чисел
-    static void openRandomSeq(QString path,
-                              QString name,
-                              int lenght);
+    static QFile* openRandomSeq(QString path,
+                                QString name,
+                                int lenght);
+
+    static void set(Runner r,
+                    QFile *file,
+                    qint64 pos);
 
     Runs();
 };
