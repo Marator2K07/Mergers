ff5e0a3df6f7f349b69e1a129e10c14350cfb676
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-10-20 09:32:44 +0300

    Создание нового класса SequenceSorting для непосредственной сортировки файлов.

---
+++ sequencesorting.cpp
@@ -0,0 +1,6 @@
+#include "sequencesorting.h"
+
+SequenceSorting::SequenceSorting()
+{
+
+}
---
+++ sequencesorting.h
@@ -0,0 +1,13 @@
+#ifndef SEQUENCESORTING_H
+#define SEQUENCESORTING_H
+
+///
+/// \brief The SequenceSorting class
+/// класс со статическими методами сортировки последовательностей
+class SequenceSorting
+{
+public:
+    SequenceSorting();
+};
+
+#endif // SEQUENCESORTING_H
