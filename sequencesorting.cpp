1316f71e3e3e3798546b887c62e3b85fb430c022
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-10-20 09:39:25 +0300

    Создание нового метода сортировки последовательностей. Начальная его реализация.

--- sequencesorting.cpp
+++ sequencesorting.cpp
@@ -1,6 +1,18 @@
 #include "sequencesorting.h"
 
+void SequenceSorting::NaturalMerge(QFile *src)
+{
+    int L; // число серий после слияния
+    QFile *f0;
+    QFile *f1;
+    QFile *f2;
+    // создание ползунков для файлов выше
+    Runner *r0;
+    Runner *r1;
+    Runner *r2;
+
+}
+
 SequenceSorting::SequenceSorting()
 {
-
 }
--- sequencesorting.h
+++ sequencesorting.h
@@ -1,12 +1,19 @@
 #ifndef SEQUENCESORTING_H
 #define SEQUENCESORTING_H
 
+#include "runs.h"
+
 ///
 /// \brief The SequenceSorting class
 /// класс со статическими методами сортировки последовательностей
 class SequenceSorting
 {
 public:
+    ///
+    /// \brief NaturalMerge
+    /// сортировка естественными слияниями
+    static void NaturalMerge(QFile *src);
+
     SequenceSorting();
 };
 
