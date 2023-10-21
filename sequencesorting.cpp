4140bc8520e9aa692d4b70e57da5e4ae13dd06f6
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-10-21 18:14:43 +0300

    Упрощенная тестовая реализация.
    Код на данный момент распределяет последовательность (файл) на два файла с сериями (отсортированными частями последовательности (файла)

--- sequencesorting.cpp
+++ sequencesorting.cpp
@@ -2,7 +2,7 @@
 
 void SequenceSorting::NaturalMerge(QFile *src)
 {
-    int L; // число серий после слияния
+    int L = -1; // число серий после слияния
     QFile *f0;
     QFile *f1;
     QFile *f2;
@@ -12,9 +12,33 @@ void SequenceSorting::NaturalMerge(QFile *src)
     Runner *r2 = new Runner;
     // инициализируем бегунок с помощью файла источника
     Runs::setRunner(r2, src, 0);
-    do {
 
-    } while (L != 1);
+    //do {
+
+        // инициализируем вспомогательные файлы
+        f0 = Runs::newFile("D:", "f0.txt");
+        f1 = Runs::newFile("D:", "f1.txt");
+        // ставим бегунки в этих файлах на нулевые позиции
+        Runs::setRunner(r0, f0, 0);
+        Runs::setRunner(r1, f1, 0);
+
+        // распределить последовательности из r2 в r0 и r1
+        do {
+            Runs::copyRun(r2, r0);
+            if (!r2->getEof()) {
+                Runs::copyRun(r2, r1);
+            }
+        } while (!r2->getEof());
+        /*
+        Runs::setRunner(r0, f0, 0);
+        Runs::setRunner(r1 ,f1, 0);
+        f2 = Runs::newFile("D:", "f2.txt");
+        Runs::setRunner(r2, f2, 0);
+
+    //} while (L != 1);
+
+    */
+
 }
 
 SequenceSorting::SequenceSorting()
