77bae41266be78ccd55a7e6735b31b4a80fb6828
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-10-22 16:30:36 +0300

    Продолжение написания алгоритма натурального слияния

--- sequencesorting.cpp
+++ sequencesorting.cpp
@@ -14,28 +14,27 @@ void SequenceSorting::NaturalMerge(QFile *src)
     Runs::setRunner(r2, src, 0);
 
     //do {
-
         // инициализируем вспомогательные файлы
         f0 = Runs::newFile("D:", "f0.txt");
         f1 = Runs::newFile("D:", "f1.txt");
         // ставим бегунки в этих файлах на нулевые позиции
         Runs::setRunner(r0, f0, 0);
         Runs::setRunner(r1, f1, 0);
-
-        // распределить последовательности из r2 в r0 и r1
+        // распределяем последовательности из r2 в r0 и r1
         do {
             Runs::copyRun(r2, r0);
             if (!r2->getEof()) {
                 Runs::copyRun(r2, r1);
             }
         } while (!r2->getEof());
-        /*
+        // подготовка перед будущим слиянием файлов
         Runs::setRunner(r0, f0, 0);
         Runs::setRunner(r1 ,f1, 0);
         f2 = Runs::newFile("D:", "f2.txt");
         Runs::setRunner(r2, f2, 0);
 
     //} while (L != 1);
+    /*
 
     */
 
