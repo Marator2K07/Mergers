d9f33f218b6d1d41d95b3670f185663e87d8670a
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-10-23 14:51:47 +0300

    Тестовый запуск алгоритма естественных слияний

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <QCoreApplication>
 
-#include "runs.h"
+#include "sequencesorting.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
     QFile *newFile = Runs::openRandomSeq("D:","outR.txt", 22);
-    Runner *r = new Runner();
-    Runs::setRunner(r, newFile, 0);
+    SequenceSorting::NaturalMerge(newFile);
 
-    int *lol = new int;
-    Runs::writeIntToRunnerPos(r, 778);
-    Runs::readIntFromRunnerPos(r, *lol);
-
-    Runs::writeSeq(newFile);
+    //Runner *r = new Runner();
+    //Runs::setRunner(r, newFile, 0);
+    //int *lol = new int;
+    //Runs::writeIntToRunnerPos(r, 778);
+    //Runs::readIntFromRunnerPos(r, *lol);
+    //Runs::writeSeq(newFile);
 
     return a.exec();
 }
