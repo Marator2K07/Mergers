f88b4e748334f9b37cc5a590f5be5c5ba0ebb5d3
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-06 19:13:18 +0300

    Тестовая временная реализация.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     QFile *newFile = Runs::openRandomSeq("D:","outR.txt", 22);
-    SequenceSorting::BalancedMerge(newFile, 4);
+    SequenceSorting::distribute(newFile, "D:", 4);
+    //QFile *result = SequenceSorting::BalancedMerge(newFile, "D:", 2);
+    //result->remove();
 
     //Runner *r = new Runner();
     //Runs::setRunner(r, newFile, 0);
